8a83490e11e60a9d6486610f276a6cc9e029cd55
Somya <somya2001p@gmail.com>
2024-06-25 22:52:02 +0530

    [Somya]added payment and bookings

---
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/bean/Booking.java
@@ -0,0 +1,76 @@
+package com.flipkart.bean;
+
+public class Booking {
+    private int bookingId;
+    private int customerId;
+    private int gymId;
+    private int transactionId;
+    private String bookingDate;
+    private String bookingTimeSlot;
+    private String bookingType;
+    private int bookingAmount;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getGymId() {
+        return gymId;
+    }
+
+    public void setGymId(int gymId) {
+        this.gymId = gymId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getBookingTimeSlot() {
+        return bookingTimeSlot;
+    }
+
+    public void setBookingTimeSlot(String bookingTimeSlot) {
+        this.bookingTimeSlot = bookingTimeSlot;
+    }
+
+    public String getBookingType() {
+        return bookingType;
+    }
+
+    public void setBookingType(String bookingType) {
+        this.bookingType = bookingType;
+    }
+
+    public int getBookingAmount() {
+        return bookingAmount;
+    }
+
+    public void setBookingAmount(int bookingAmount) {
+        this.bookingAmount = bookingAmount;
+    }
+}
