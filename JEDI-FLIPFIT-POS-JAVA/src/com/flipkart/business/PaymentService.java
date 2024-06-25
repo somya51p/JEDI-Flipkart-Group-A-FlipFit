286e22c18a4f02ded3820efbb4520c04bd114230
Sarthak Vats <2020ceb1027@iitrpr.ac.in>
2024-06-25 23:31:06 +0530

    Added payment services

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/PaymentService.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/PaymentService.java
@@ -1,5 +1,19 @@
 package com.flipkart.business;
 
+import com.flipkart.bean.Payment;
+
 public class PaymentService {
-      
+
+      Payment payment = new Payment();
+
+      public void createPayment(int transactionId, int cardNumber, String expiryDate, String modeOfPayment)
+      {
+            payment.setTransactionId(transactionId);
+            payment.setCardNumber(cardNumber);
+            payment.setExpiryDate(expiryDate);
+            payment.setModeOfPayment(modeOfPayment);
+
+            System.out.println("Payment Successful!");
+      }
+
 }
