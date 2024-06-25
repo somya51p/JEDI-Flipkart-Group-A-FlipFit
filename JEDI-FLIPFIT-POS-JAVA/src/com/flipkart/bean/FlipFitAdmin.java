3914233c1536986d31a3219d308a9c38664477d7
Somya <somya2001p@gmail.com>
2024-06-25 21:54:27 +0530

    [Somya]added admin bean

---
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/bean/FlipFitAdmin.java
@@ -0,0 +1,32 @@
+package com.flipkart.bean;
+
+public class FlipFitAdmin {
+    private int id;
+    private String adminEmail;
+    private String adminPassword;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+}
