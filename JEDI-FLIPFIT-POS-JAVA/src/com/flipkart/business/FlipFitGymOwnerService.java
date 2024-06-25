a3da2803d23bb1f9fca1709e8753a5dcdf0d72ea
Sahas <byri.sahasreddy@flipkart.com>
2024-06-26 00:18:15 +0530

    first commit

---
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/FlipFitGymOwnerService.java
@@ -0,0 +1,66 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.FlipFitGymOwner;
+import com.flipkart.business.FlipFitGymService;
+
+public class FlipFitGymOwnerService {
+
+    FlipFitGymOwner gymOwner = new FlipFitGymOwner();
+    public void createGymOwner(int id, String name, String email, String phone, String address, String pan_no, String gst_no, String status) {
+
+        gymOwner.setOwnerId(id);
+        gymOwner.setOwnerName(name);
+        gymOwner.setOwnerPanNum(pan_no);
+        gymOwner.setOwnerEmail(email);
+        gymOwner.setOwnerPhone(phone);
+        gymOwner.setOwnerAddress(address);
+        gymOwner.setOwnerGstNum(gst_no);
+        gymOwner.setApprovalStatus(status);
+
+        System.out.println("gym owner details added");
+    }
+
+    public void editProfile(int id, String name, String email, int phone, String password, String address, String pan_no, String gst_no, String status) {
+        System.out.println("edited gym owner details");
+    }
+
+    public void registerGym(int gymId, String name, String location) {
+        FlipFitGymService gymService = new FlipFitGymService();
+        gymService.createGym(gymId, gymOwner.getOwnerId(), name, location);
+
+        System.out.println("gym registered successfully");
+    }
+
+    public void editGym(int gymId) {
+        System.out.println("gym " + gymId + "edited successfully");
+    }
+
+    public void removeGym(int gymId) {
+        System.out.println("gym " + gymId + "removed successfully");
+    }
+
+    public void viewAllRegisteredGymCenters() {
+        System.out.println("view All Gym Centers");
+    }
+
+    public void viewAllBookings() {
+        System.out.println("view all bookings");
+    }
+
+    public void viewBookings(int gymId) {
+        System.out.println("view bookings for " + gymId);
+    }
+
+    public void viewAvailableSlots(int gymId) {
+        System.out.println("view available slots for " + gymId);
+    }
+
+    public void addSlot(int gymId, int slotId) {
+        System.out.println("Added slot " + slotId + "for gym " + gymId);
+    }
+
+    public void removeSlot(int gymId, int slotId) {
+        System.out.println("Removed slot " + slotId + "for gym " + gymId);
+    }
+
+}
