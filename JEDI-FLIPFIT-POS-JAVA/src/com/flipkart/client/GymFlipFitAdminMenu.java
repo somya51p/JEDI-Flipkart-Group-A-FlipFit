ab10a75732f9fee425f9817944cf189bd094a952
Aman Harsh <aman.harsh.1451@gmail.com>
2024-06-26 13:03:39 +0530

    Clien page for Customer and Admin

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/client/GymFlipFitAdminMenu.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/client/GymFlipFitAdminMenu.java
@@ -1,23 +1,43 @@
 package com.flipkart.client;
 
-import com.flipkart.business.FlipfitAdminInterface;
+import com.flipkart.business.FlipfitAdminInterface ;
 import com.flipkart.business.FlipfitAdminService;
 
+import java.util.Scanner;
+
 public class GymFlipFitAdminMenu {
+	public static void displayAdminOptions(String[] args) {
+		FlipfitAdminInterface adminService = new FlipfitAdminService() ;
+		boolean flag= true ;
+		do {
+			System.out.println("Welcome to admin page :");
+			System.out.println("1. View all details of gym owner : \n2. View requests of gym owners : \n3. Approval of gym owner requests : \n4. Remove any gym owner : \n 5. Cancel any pending request : \n6. Exit");
+			Scanner sc = new Scanner(System.in);
+			int option = sc.nextInt();
+			switch (option) {
+				case 1:
+					adminService.viewGymOwnerDetails(101);
+					break;
+				case 2:
+					adminService.viewGymOwnerRequests();
+					break;
+				case 3:
+					adminService.approveGymOwnerRequests(101);
+					break;
+				case 4:
+					adminService.removeGymOwner(101);
+					break;
+				case 5:
+					adminService.cancelRequest(101);
+					break;
+				case 6:
+					System.out.println("Exit");
+					flag= false ;
+					break;
+				default:
+					System.out.println("invalid option");
+			}
+		}while(flag);
 
-	public static void main(String[] args) {
-		FlipfitAdminInterface adminService = new FlipfitAdminService();
-
-		adminService.createAdmin(1, "admin@example.com", "admin@123");
-
-		adminService.viewGymOwnerDetails(101);
-
-		adminService.viewGymOwnerRequests();
-
-		adminService.approveGymOwnerRequests(101);
-
-		adminService.removeGymOwner(102);
-
-		adminService.cancelRequest(103);
 	}
 }
--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/client/GymFlipFitCustomerMenu.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/client/GymFlipFitCustomerMenu.java
@@ -3,20 +3,55 @@ import com.flipkart.business.BookingGymInterface;
 import com.flipkart.business.BookingGymService;
 import com.flipkart.business.FlipFitCustomerInterface;
 import com.flipkart.business.FlipFitCustomerService;
+
+import java.util.*;
+
 public class GymFlipFitCustomerMenu {
    
-	public static void main(String[] Args){
-
+	public static void displayCustomerOptions(String[] Args){
 		FlipFitCustomerInterface userService = new FlipFitCustomerService();
 		BookingGymInterface bookingService = new BookingGymService();
-		userService.createCustomer(101,"Sarthak","sarthak.vats@gmail.com", "987","ABC", "XYZ" );
-		userService.editProfile();
-		userService.viewGyms();
-		userService.viewSlots();
-		userService.filterSlots();
-		bookingService.bookSlots();
-		bookingService.makePayment();
-		bookingService.viewBookings();
-		bookingService.cancelBookings();
+
+		boolean flag = true;
+
+		do{
+			System.out.println("Welcome to FlipFit Customer Page");
+			System.out.println("Customer Menu: \n1. Edit your Profile\n2. View all Gyms\n3. View available Slots\n 4. Filter Slots\n5. Book your slot\n6. View your bookings\n7. Cancel your bookings\n8. Exit");
+
+			Scanner in = new Scanner(System.in);
+			int i = in.nextInt();
+
+			switch (i) {
+				case 1:
+					userService.editProfile();
+					break;
+				case 2:
+					userService.viewGyms();
+					break;
+				case 3:
+					userService.viewSlots();
+					break;
+				case 4:
+					userService.filterSlots();
+					break;
+				case 5:
+					bookingService.bookSlots();
+					bookingService.makePayment(); //it should come under book slot
+					break;
+				case 6:
+					bookingService.viewBookings();
+					break;
+				case 7:
+					bookingService.cancelBookings();
+					break;
+				case 8:
+					System.out.println("Thank you for using FlipFit Customer Page");
+					flag = false;
+					break;
+				default:
+					System.out.println("Invalid option");
+			}
+		} while(flag);
+		
 	  }
 }
