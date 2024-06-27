ab3a77e56a0a992b088f8caeea6c7e5e97442007
Sarthak Vats <2020ceb1027@iitrpr.ac.in>
2024-06-27 14:35:28 +0530

    First commit

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipFitCustomerDAOInterface.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipFitCustomerDAOInterface.java
@@ -2,7 +2,7 @@ package com.flipkart.dao;
 
 public interface FlipFitCustomerDAOInterface {
     public void createCustomer(int customerId, int userId, String name, String phoneNumber, String address, String userEmail, String userPass);
-    public void editProfile();
+    public void editProfile(int customerId, String customerName, String customerPhone, String customerAddress) ;
     public void viewGyms();
     public void viewSlots();
     public void filterSlots();
