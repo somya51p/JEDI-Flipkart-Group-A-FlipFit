5369cab66872dcc5d452daf3dc98d062979ccc26
Sahas <byri.sahasreddy@flipkart.com>
2024-06-27 16:07:26 +0530

    resolved conflict

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipFitGymOwnerDAOImpl.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/dao/FlipFitGymOwnerDAOImpl.java
@@ -1,9 +1,6 @@
 package com.flipkart.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class FlipFitGymOwnerDAOImpl implements FlipFitGymOwnerDAOInterface{
     @Override
