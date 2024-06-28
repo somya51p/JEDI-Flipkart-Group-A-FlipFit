0041847837952a6a26f2f9eb5761052e286eefb8
Sarthak Vats <2020ceb1027@iitrpr.ac.in>
2024-06-28 15:25:48 +0530

    added viewSlots in Customer

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/FlipFitCustomerService.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/FlipFitCustomerService.java
@@ -4,9 +4,17 @@ import com.flipkart.dao.FlipFitCustomerDAOImpl;
 import com.flipkart.dao.FlipFitCustomerDAOInterface;
 import com.flipkart.exceptions.UserNotFoundException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlipFitCustomerService implements FlipFitCustomerInterface {
 
     FlipFitCustomerDAOInterface customerDAO = new FlipFitCustomerDAOImpl();
+
+    public static void main(String[] args) {
+        FlipFitCustomerInterface customerService = new FlipFitCustomerService();
+        customerService.viewSlots();
+    }
 
     public void createCustomer(int userId, String name, String phoneNumber, String address) {
         customerDAO.createCustomer(userId, name, phoneNumber, address);
@@ -24,8 +32,16 @@ public class FlipFitCustomerService implements FlipFitCustomerInterface {
     }
 
     public void viewSlots(){
+        int gymId=1;
+        String date="228/06/2024";
         System.out.println("All slots are viewed");
-    }
+        // Print the map
+        HashMap<String,Integer>AvailableSlots=customerDAO.viewSlots(gymId,date);
+        // Print the available slots
+        for (Map.Entry<String, Integer> entry : AvailableSlots.entrySet()) {
+            System.out.println("Slot Time: " + entry.getKey() + ", Available Slots: " + entry.getValue());
+        }
+        }
 
     public void filterSlots(){
         System.out.println("All slots are filtered");
