592e01fc6c442a1b4e1a49e0ff42875743612fd7
Aman Harsh <aman.harsh.1451@gmail.com>
2024-06-28 12:15:38 +0530

    added dao callings in admin service

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/FlipfitAdminService.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/FlipfitAdminService.java
@@ -2,43 +2,54 @@ package com.flipkart.business;
 import com.flipkart.bean.FlipFitAdmin;
 import com.flipkart.bean.Roles;
 import com.flipkart.bean.Users;
+import com.flipkart.dao.FlipFitCustomerDAOImpl;
+import com.flipkart.dao.FlipFitCustomerDAOInterface;
+import com.flipkart.dao.FlipfitAdminDAOImpl;
+import com.flipkart.dao.FlipfitAdminDAOInterface;
+
 
 public class FlipfitAdminService implements FlipfitAdminInterface{
         FlipFitAdmin flipfitadmin = new FlipFitAdmin();
         Users user= new Users();
 
+        FlipfitAdminDAOInterface adminDAO = new FlipfitAdminDAOImpl();
+
         public void createAdmin(int adminId, int userId, String userEmail, String userPass){
 
-              flipfitadmin.setUserId(userId);
+            flipfitadmin.setUserId(userId);
+            user.setUserEmail(userEmail);
+            user.setUserPassword(userPass);
 
-              user.setUserEmail(userEmail);
-              user.setUserPassword(userPass);
+            user.setRoleId(3);
+            flipfitadmin.setAdminId(adminId);
+            user.setUserId(userId);
 
-              user.setRoleId(3);
-              flipfitadmin.setAdminId(adminId);
-              user.setUserId(userId);
-
-              System.out.println("Admin created");
+            System.out.println("Admin created");
         }
 
         public boolean viewGymOwnerDetails(int ownerId){
-              System.out.println("View All the approved gym owner details");
-              return true;
+            adminDAO.viewGymOwnerDetails(ownerId);
+            System.out.println("View All the approved gym owner details");
+            return true;
         }
         public boolean viewGymOwnerRequests(){
-                System.out.println("View all the gym owner pending requests");
-                return true;
+            adminDAO.viewGymOwnerRequests();
+            System.out.println("View all the gym owner pending requests");
+            return true;
         }
         public boolean approveGymOwnerRequests(int ownerId){
-                System.out.println("Approve the gym owner requests with Id "+ ownerId);
-                return true;
+            adminDAO.approveGymOwnerRequests(ownerId);
+            System.out.println("Approve the gym owner requests with Id "+ ownerId);
+            return true;
         }
         public boolean removeGymOwner(int ownerId){
-                System.out.println("Remove the gym owner with Id "+ ownerId);
-                return true;
+            adminDAO.removeGymOwner(ownerId);
+            System.out.println("Remove the gym owner with Id "+ ownerId);
+            return true;
         }
         public boolean cancelRequest(int ownerId) {
-                System.out.println("Cancel the gym owner request with Id " + ownerId);
-                return true;
+            adminDAO.cancelRequest(ownerId);
+            System.out.println("Cancel the gym owner request with Id " + ownerId);
+            return true;
         }
 }
