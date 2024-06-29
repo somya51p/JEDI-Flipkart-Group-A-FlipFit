c677b29aa77d45bc388b068279fbed09a4c0c2fc
Sarthak Vats <2020ceb1027@iitrpr.ac.in>
2024-06-29 10:41:10 +0530

    Implemented BookingFailedException and GymNotFoundException

--- JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingGymInterface.java
+++ JEDI-FLIPFIT-POS-JAVA/src/com/flipkart/business/BookingGymInterface.java
@@ -4,7 +4,7 @@ import com.flipkart.exceptions.BookingFailedException;
 
 public interface BookingGymInterface {
 
-    public void createBooking(int userId, int gymId, int transactionId, String bookingDate, String bookingTimeSlot, String bookingType, int bookingAmount) throws BookingFailedException;
+    public void createBooking(int userId, int gymId, int transactionId, String bookingDate, String bookingTimeSlot, String bookingType, int bookingAmount);
     public void bookSlots() throws BookingFailedException;
     public void viewBookings(int userId) throws BookingFailedException;
     public void cancelBookings(int bookingId) throws BookingFailedException;
