7e30ec858a5957a6142cde241765391e529fdba4
Sarthak Vats <2020ceb1027@iitrpr.ac.in>
2024-06-30 15:49:49 +0530

    Implemented customer Controller functions

---
+++ JEDI-FLIPFIT-GROUP-A-DROPWIZARD/src/main/java/com/flipkart/business/BookingGymService.java
@@ -0,0 +1,68 @@
+package com.flipkart.business;
+
+import com.flipkart.bean.Booking;
+import com.flipkart.dao.BookingGymDAOImpl;
+import com.flipkart.dao.BookingGymDAOInterface;
+import com.flipkart.dao.FlipFitCustomerDAOImpl;
+import com.flipkart.dao.FlipFitCustomerDAOInterface;
+import com.flipkart.exceptions.BookingFailedException;
+
+import java.util.List;
+
+
+
+public class BookingGymService implements BookingGymInterface {
+
+    BookingGymDAOInterface bookingGymDAO = new BookingGymDAOImpl();
+
+    public void createBooking(int userId, int gymId, int transactionId, String bookingDate, String bookingTimeSlot, String bookingType, int bookingAmount) {
+        try{
+            bookingGymDAO.createBooking(userId, gymId, transactionId, bookingDate, bookingTimeSlot, bookingType, bookingAmount);
+            System.out.println("Booking is Done!!");
+        }
+        catch(BookingFailedException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void bookSlots() throws BookingFailedException{
+        System.out.println("Slot is booked");
+    }
+
+    public List<Booking> viewBookings(int userId){
+        List<Booking> bookings = bookingGymDAO.viewBookings(userId);
+
+//        if (bookings.isEmpty()) {
+//            System.out.println("No bookings found for userId: " + userId);
+//        } else {
+//            System.out.println("Bookings for userId: " + userId);
+//            for (Booking booking : bookings) {
+//                System.out.println("Booking ID: " + booking.getBookingId());
+//                System.out.println("Customer ID: " + booking.getCustomerId());
+//                System.out.println("Gym ID: " + booking.getGymId());
+//                System.out.println("Transaction ID: " + booking.getTransactionId());
+//                System.out.println("Booking Date: " + booking.getBookingDate());
+//                System.out.println("Booking TimeSlot: " + booking.getBookingTimeSlot());
+//                System.out.println("Booking Type: " + booking.getBookingType());
+//                System.out.println("Booking Amount: " + booking.getBookingAmount());
+//                System.out.println("=================================");
+//            }
+        return bookings;
+    }
+
+public void cancelBookings(int bookingId){
+    bookingGymDAO.cancelBookings(bookingId);
+    System.out.println("Booking is cancelled");
+}
+
+public int makePayment(int userId, String paymentDetails, String expiryDate, String modeOfPayment) throws BookingFailedException{
+    try{
+        System.out.println("Payment Successful!");
+        return bookingGymDAO.makePayment(userId, paymentDetails, expiryDate, modeOfPayment);
+    }
+    catch(BookingFailedException e){
+        throw new BookingFailedException(e.getMessage());
+    }
+}
+
+}
---
+++ JEDI-FLIPFIT-GROUP-A-DROPWIZARD/src/main/java/com/flipkart/rescontroller/CustomerController.java
@@ -0,0 +1,135 @@
+package com.flipkart.rescontroller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flipkart.bean.Booking;
+import com.flipkart.bean.FlipFitGym;
+import com.flipkart.business.*;
+import com.flipkart.exceptions.BookingFailedException;
+import com.flipkart.exceptions.GymNotFoundException;
+import com.flipkart.exceptions.UserNotFoundException;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.List;
+
+
+@Path("/customer")
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomerController {
+    FlipFitCustomerInterface customerService = new FlipFitCustomerService();
+    BookingGymInterface booking = new BookingGymService();
+
+    int userId = -1;
+
+    //http://localhost:8080/customer/login?email=customer@example.com&password=password123
+    @GET
+    @Path("/login")
+    public Response customerLogin(@QueryParam("email") String email, @QueryParam("password") String password) {
+        FlipFitUserInterface user = new FlipFitUserService();
+        userId = user.authenticateUser(email, password, 1);
+        if (userId > 0) {
+            System.out.println("Logged in as Customer");
+            return Response.ok("Successfully Logged In as a Customer").build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid Email or Password").build();
+        }
+    }
+
+    //http://localhost:8080/customer/edit-profile?name=tgtdrsdrsyz&phoneNumber=4992302&address=dsqtgf
+    @GET
+    @Path("/edit-profile")
+    public Response editProfile(@QueryParam("name") String name, @QueryParam("phoneNumber") String phoneNumber, @QueryParam("address") String address) {
+        try {
+            customerService.editProfile(userId, name, phoneNumber, address);
+        } catch (UserNotFoundException e) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity(e.getMessage()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+        return Response.ok("Successfully Editted the Profile").build();
+    }
+
+    //http://localhost:8080/customer/view-gyms
+    @GET
+    @Path("/view-gyms")
+    public Response viewGyms() {
+        List<FlipFitGym> gyms = customerService.viewGyms();
+        try {
+            if (userId <= 0)
+                return Response.status(Response.Status.UNAUTHORIZED).entity("You are not an authorized user!").build();
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(gyms);
+            return Response.ok("All gyms are listed below:\n" + json).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error converting gym list to JSON").build();
+        }
+    }
+
+    //http://localhost:8080/customer/view-slots?gymId=1&date=20-07-2024
+    @GET
+    @Path("/view-slots")
+    public Response viewSlots(@QueryParam("gymId") int gymId, @QueryParam("date") String date) {
+        try {
+            if (userId <= 0)
+                return Response.status(Response.Status.UNAUTHORIZED).entity("You are not an authorized user!").build();
+            HashMap<String, Integer> slots = customerService.viewSlots(gymId, date);
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(slots);
+            return Response.ok("All slots of gymId: " + gymId + " on date: " + date + " are listed below:\n" + json).build();
+        } catch (GymNotFoundException e) {
+            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    //http://localhost:8080/customer/create-booking?gymId=1&bookingDate=01-07-2024&bookingTime=07:00
+    @GET
+    @Path("/create-booking")
+    public Response createBooking(@QueryParam("gymId") int gymId, @QueryParam("bookingDate") String bookingDate, @QueryParam("bookingTime") String bookingTime) {
+        try {
+            if (userId <= 0)
+                return Response.status(Response.Status.UNAUTHORIZED).entity("You are not an authorized user!").build();
+            int transactionId = booking.makePayment(userId, "Rs. 1000 paid Successfully", "10-05-2030", "Net-Banking");
+            booking.createBooking(userId, gymId, transactionId, bookingDate, bookingTime, "Confirmed", 1000);
+            return Response.ok("Successfully created booking").build();
+        } catch (BookingFailedException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    //http://localhost:8080/customer/view-bookings
+    @GET
+    @Path("/view-bookings")
+    public Response viewBookings() {
+        try {
+            if (userId <= 0)
+                return Response.status(Response.Status.UNAUTHORIZED).entity("You are not an authorized user!").build();
+            List<Booking> bookingList = booking.viewBookings(userId);
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(bookingList);
+            return Response.ok("All bookings are listed below:\n" + json).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+
+    //http://localhost:8080/customer/cancel-booking?bookingId=1
+    @GET
+    @Path("/cancel-booking")
+    public Response cancelBooking(@QueryParam("bookingId") int bookingId) {
+        try {
+            if (userId <= 0) {
+                return Response.status(Response.Status.UNAUTHORIZED).entity("You are not an authorized user!").build();
+            }
+            booking.cancelBookings(bookingId);
+            return Response.ok("Booking cancelled").build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+        }
+    }
+}
