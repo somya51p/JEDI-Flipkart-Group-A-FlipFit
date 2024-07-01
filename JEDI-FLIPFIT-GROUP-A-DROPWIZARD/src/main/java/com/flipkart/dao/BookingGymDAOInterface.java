9379595a20ba9cc83c9e86738b564a0c0f372edd
rupasreeyeduru <yeduru.rupasree@students.iiit.ac.in>
2024-07-01 10:08:44 +0530

    added Exceptions

--- JEDI-FLIPFIT-GROUP-A-DROPWIZARD/src/main/java/com/flipkart/dao/BookingGymDAOInterface.java
+++ JEDI-FLIPFIT-GROUP-A-DROPWIZARD/src/main/java/com/flipkart/dao/BookingGymDAOInterface.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public interface BookingGymDAOInterface {
 
-    public void createBooking(int userId, int gymId, int transactionId, String bookingDate, String bookingTimeSlot, String bookingType, int bookingAmount) throws BookingFailedException;
+    public void createBooking(int userId, int gymId, int transactionId, String bookingDate, String bookingTimeSlot, String bookingType, int bookingAmount) throws Exception;
     public void bookSlots() throws BookingFailedException;
     public List<Booking> viewBookings(int userId);
-    public void cancelBookings(int bookingId);
+    public void cancelBookings(int bookingId) throws Exception;
     public int makePayment(int userId, String paymentDetails, String expiryDate, String modeOfPayment) throws BookingFailedException;
 }
---
+++ JEDI-FLIPFIT-GROUP-A-DROPWIZARD/src/main/java/com/flipkart/exceptions/BookingNotFoundException.java
@@ -0,0 +1,7 @@
+package com.flipkart.exceptions;
+
+public class BookingNotFoundException extends Exception{
+     public BookingNotFoundException(String message){
+            super(message);
+        }
+    }
